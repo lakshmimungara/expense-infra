be69bdd8e22123b40d67ff9a551cfb40b75d2aa3
lakshmimungara <lakshmimungara2997@gmail.com>
2024-11-29 19:06:47 +0530

    jenkins

--- 40-eks/main.tf
+++ 40-eks/main.tf
@@ -1,6 +1,6 @@
 
 resource "aws_key_pair" "eks" {
-  key_name   = "linux"
+  key_name   = "lakshmi"
   # you can paste the public key directly like this
   #public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL6ONJth+DzeXbU3oGATxjVmoRjPepdl7sBuPzzQT2Nc sivak@BOOK-I6CR3LQ85Q"
   # public_key = file("~/.ssh/lakshmi.pub")
